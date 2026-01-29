f2565db7525fb1c2c1b3f0d86d448347ee75812e
chavhan3472 <sahilchavhan250@gmail.com>
2026-01-29 10:55:19 +0530

    Update db.py

--- db.py
+++ db.py
@@ -5,6 +5,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="xxxxx",      
+        password="sahil1234",      
         database="student_db"
     )
